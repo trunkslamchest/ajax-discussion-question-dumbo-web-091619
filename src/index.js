ed01cf42d0b50e7d58071e12a79c6e4fe3d18d16
Austin Smith <trunk9slamchest@gmail.com>
2019-10-31 19:14:12 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const fullname = document.getElementById("fullname");
 console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("CONTENT LOADED!");
+  console.log("CONTENT LOADED");
 });
